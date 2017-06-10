22933f2b9744cfb1977357d8eb52a1f0441e017d
aidan-n <aidan-n@users.noreply.github.com>
2017-06-10 12:07:01 -0700

    Create find_text.py

--- misc/find_text.py
+++ misc/find_text.py
@@ -1,5 +1,5 @@
-from __future__ import print_function
-from builtins import str
+#from __future__ import print_function
+#from builtins import str
 ####
 import lxml
 import requests
@@ -17,12 +17,17 @@ text_page = web_page.text
 #set up the soup. (I don't totally get what lxml does, but it works.)
 soup = BeautifulSoup(text_page, "lxml")
 
-
+#used to print a message if the word doesn't exist on the page.
 word_exists = False
+
+#iterate through html elements on the webpage
 for elem in soup(text=re.compile(r'gunshot')):
     word_exists = True
+
+    #prints the context every time we find the word
     print ('\n' + str(elem.parent))
     print("instance found: " + "gunshot\n")
 
+#if the word was not found, it never got marked as true
 if (word_exists == False):
 	print("\"gunshot\" not found")
